d118fa757a08b2c60e09edff13790dcc7b8ef2b2
Y2Kiwy <simoncello.anto@gmail.com>
2024-09-09 12:16:10 +0200

    Added protocol recognizer function (IP - TCP)

--- Lessons/03/sniffer/mysniffer.py
+++ Lessons/03/sniffer/mysniffer.py
@@ -1,8 +1,8 @@
-from scapy.packet import Packet   # Import the base Packet class
-from scapy.layers.inet import IP  # Import the IP layer
-from scapy.sendrecv import sniff  # Import the sniff function
+from scapy.packet import Packet        # Import the base Packet class
+from scapy.layers.inet import IP, TCP  # Import the IP and TCP layers
+from scapy.sendrecv import sniff       # Import the sniff function
 
-# Initialize a global variable to count the number of TCP packets read
+# Initialize a global variable to count the number of TCP packets processed
 iPkt: int = 0
 
 # Define a function to process each captured packet
@@ -14,17 +14,37 @@ def process_packet(pkt: Packet):
     iPkt += 1
     
     # Print a message showing the number of TCP packets read
-    print(f"I've read a TCP packet on your PC: {iPkt}")
+    print(f"I've read a packet on your PC: {iPkt}")
 
-    # Check if the packet contains an IP layer, return if it doesn't
+    # Check if the packet contains an IP layer; return if it doesn't
     if not pkt.haslayer(IP):
         return
-    
-    # Extract the source and destination IP addresses and the protocol, then store them in a string
-    ip_layer: str = f"IP_SRC: {pkt[IP].src} - IP_DST: {pkt[IP].dst} - PROTO: {pkt[IP].proto} - IP_LEN: {pkt[IP].len}\n"
-    
-    # Print the IP source, destination, and protocol information
-    print(ip_layer)
 
-# Start sniffing TCP packets on the "eth0" interface and call process_packet on each captured packet
+    # Check if the packet contains a TCP layer
+    if pkt[IP].proto == 6:
+        # Extract TCP source and destination ports and store them in a string
+        tcp_layer: str = f"TCP_SPORT: {pkt[TCP].sport} - TCP_DPORT: {pkt[TCP].dport}"
+
+        # Print the TCP source and destination ports
+        print(tcp_layer)
+
+        # Check if the packet is HTTP
+        if pkt[IP].sport == 80 or pkt[IP].dport == 80:
+            print("It's an HTTP packet\n")
+        
+        # Check if the packet is HTTPS (TLS)
+        elif pkt[IP].sport == 443 or pkt[IP].dport == 443:
+            print("It's an HTTPS (TLS) packet\n")
+
+        else:
+            print()
+
+    else:
+        # Extract IP source and destination addresses and protocol information, then store them in a string
+        ip_layer: str = f"IP_SRC: {pkt[IP].src} - IP_DST: {pkt[IP].dst} - PROTO: {pkt[IP].proto} - IP_LEN: {pkt[IP].len}\n"
+
+        # Print the IP source, destination, protocol, and length information
+        print(ip_layer)
+
+# Start sniffing TCP packets on the "eth0" interface and call process_packet for each captured packet
 sniff(iface="eth0", filter="tcp", prn=process_packet)
